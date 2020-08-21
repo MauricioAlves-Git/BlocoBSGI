51de49922abf412339dcdbcc6e85e5adf32b624d
BrenoGdS <breno.eletronica@gmail.com>
2020-08-20 23:42:18 -0300

    criação da arquitetura básica do projeto. Precisamos revisar e cobrar do professor que nos ensine uma estrutura MVC padrão para PHP.

---
+++ frontend/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+      <link href="src/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	  <title>Bloco BSGI</title>
+	  <meta charset="utf-8">
+	</head>
+	<body>
+
+
+    <!-- fim modal dois -->
+    <header id="header">
+
+
+    </header><!-- End Header -->
+
+
+	</body>
+</html>
