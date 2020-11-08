7afd42b32da7cb01940eac6e1293ccce9014f0e1
RodrigoBrittes <73858610+RodrigoBrittes@users.noreply.github.com>
2020-11-08 12:09:40 -0300

    novas versoes dos arquivos
    alteraçoes nos arquivos: estilo1.css, form_inserc_org.php e index-org.php. Formulário e layout melhorados.

---
+++ frontend/org/form_inserc_org-v2.2.php
@@ -0,0 +1,179 @@
+<!HTML>
+
+<!-- form_inserc_org.php 
+     formulario de insercao das organizaçoes
+<!-- versao 2.2 - baseado em cadastro_bd_pdo (form-insercao.php) -->
+
+<html lang="pt-br">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="estilo1-v2.2.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <title> BSGI - Organização </title>
+  <!--
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	-->	
+    </head>
+
+<body>
+    <header>
+  
+  	<div id="cab_esq">
+		<img class="contain" src="..\src\img\BSGI-logo2c.png" align=left width="70%">
+	</div>
+	<div id="cab_dir">
+		<!-- <img src="img\icon3-user.png" align=left> ENORME -->
+	</div>
+    </header>
+
+    <nav id="navegar"> 
+	<a href="#" class="opcoes">Perfil</a>
+	<a href="#" class="opcoes">Info</a>
+	<a href="#" class="opcoes">Instagram</a>
+	<a href="#" class="opcoes">Facebook</a>
+	<a href="#" class="opcoes">Home</a>
+    </nav>
+   
+
+    <section id="barra"> </section>
+    <section id="main"> 
+        <h1 class="titulo">Cadastro Organizacional   - - - - - - - - - - - - - - - - - - -   Consultar - Incluir - Excluir</h1>
+
+        
+    <form action="insercao_organizacao.php" method="GET">
+    <div id="div1">
+	<label for="idNomeOrg">
+		<input type="text" name="nome_org" id="idNomeOrg" placeholder="Nome da Organização" class="form-control mb-1 mr-sm-2">
+	</label>
+	<br>        
+	<label for="idNomeOrgPai">
+		<input type="text" name="id_organizacao_pai" id="idNomeOrgPai" placeholder="Organização-Pai (selecione)" 
+                 class="form-control mb-1 mr-sm-2">
+	</label>
+	<br>
+        <label for="idTipoOrg"> 
+        <select name="TipoOrg" id="idTipoOrg" class="form-control mb-1 mr-sm-2">
+           
+		<!-- <input type="text" select id="id_tipo_org" name="TipoOrg" placeholder="Tipo da Organização (selecione)" 
+                 class="form-control mb-1 mr-sm-2"> -->
+            <optgroup label="Tipo da Organização:">
+            <option value="">Tipo da Organização (selecione)</option> 
+            <option value="Bloco">Bloco</option>
+            <option value="Comunidade">Comunidade</option>
+            <option value="Distrito">Distrito</option>
+            </optgroup>
+	</select>
+    </div>
+    <br>
+
+        
+    <div id="div2">
+           
+        <label for="idUFOrg"></label>
+                <!-- <input type="text" name="FALTANDO" id="idUFOrg" placeholder="Nome da Organização"> -->
+        <br>
+        <label for="idCEPOrg">
+        	<input type="number" name="CEP_org" id="idCEPOrg" placeholder="CEP ()" class="form-control mb-1 mr-sm-2">
+        </label>
+        <br>        
+        <label for="idCidadeOrg">
+        	<input type="text" name="id_cidade_org" id="idCidadeOrg" placeholder="Cidade ()" class="form-control mb-1 mr-sm-2">
+        </label>
+        <br>
+	<label for="idLogradouroOrg">
+		<input type="text" name="logradouro_org" id="idLogradouroOrg" placeholder="Logradouro ()" class="form-control mb-1 mr-sm-2">
+	</label>	
+	<br>
+        <label for="idNumLogradouroOrg">
+            <input type="number" name="num_org" id="idLogradouroOrg" placeholder="Número ()" class="form-control mb-1 mr-sm-2">
+        </label>
+        <br>
+        <label for="idComplementoOrg">
+        	<input type="text" name="complemento_org" id="idComplementoOrg" placeholder="Complemento ()" 
+                 class="form-control mb-1 mr-sm-2">
+        </label>        
+        <br>
+        <label for="idBairroOrg">
+        	<input type="text" name="bairro_org" id="idBairroOrg" placeholder="Bairro ()" class="form-control mb-1 mr-sm-2">
+        </label>
+        <br>
+        
+    </div>
+    <br>
+        
+
+    <div id="div3">        
+	<label for="idEmailOrg">
+		<input type="email" name="EmailOrg" id="idEmailOrg" placeholder="E-mail da Organização ()" 
+                 class="form-control mb-1 mr-sm-2">
+	</label>	
+	<br>
+	<label for="idFoneFixoOrg">
+		<input type="tel" name="FoneFixoOrg" id="idFoneOrg" placeholder="Fone fixo da Organização ()" 
+                 class="form-control mb-1 mr-sm-2">
+	</label>
+        <br>
+        <label for="idFoneCelOrg">
+		<input type="tel" name="FoneCelOrg" id="idFoneOrg" placeholder="Celular da Organização ()" 
+                 class="form-control mb-1 mr-sm-2">
+	</label>
+	<br> 
+    </div>
+
+        
+        
+        
+        
+<?php
+
+    # cria conexão com banco de dados
+    //include_once "../../inc/conectabd.php"; // ativar o database
+        //include_once "../inc/funcoes.inc.php";  //VERIFICAR
+        //monta_organizacao($conn);               // CRIAR FUNÇAO
+ 
+
+    ?>
+    </form>
+        
+        
+        
+        
+</body>
+</html>
+  
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * and open the template in the editor.
+ */
+
+
+
+
+/*
+  try {
+   
+  include_once "../inc/conectabd.inc.php";
+
+  echo "<h3>BSGI (PDO)</h3>";
+  
+  
+ 
+  
+  
+    } catch (PDOException $e){
+      echo "Erro: ". $e->getMessage();
+      }
+ * 
+ */
+?> 
---
+++ frontend/org/index-org-v2.2.php
@@ -0,0 +1,210 @@
+<!HTML>
+
+<!-- index-org-v2.2.php -->
+<!-- versao 2.2 - baseado em cadastro_bd_pdo (form-insercao.php) -->
+
+<html lang="pt-br">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="estilo1-v2.2.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <title> BSGI - Organização </title>
+  <!--
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	-->	
+    </head>
+
+<body>
+    <header>
+  
+  	<div id="cab_esq">
+		<img class="contain" src="..\src\img\BSGI-logo2c.png" align=left width="70%">
+	</div>
+	<div id="cab_dir">
+		<!-- <img src="img\icon3-user.png" align=left> ENORME -->
+	</div>
+    </header>
+
+    <nav id="navegar"> 
+	<a href="#" class="opcoes">Perfil</a>
+	<a href="#" class="opcoes">Info</a>
+	<a href="#" class="opcoes">Instagram</a>
+	<a href="#" class="opcoes">Facebook</a>
+	<a href="#" class="opcoes">Home</a>
+    </nav>
+   
+    
+
+    <section id="barra"> </section>
+    <section id="main"> 
+        <h1 class="titulo">Cadastro Organizacional   - - - - - - - - - - - - - - - - - - -<span>   Consultar - Incluir - Excluir</span></h1>
+        
+    <!-- <nav id="crud" class="navbar navbar-expand-sm bg-primary navbar-dark"> -->
+    
+    <!--
+    <nav id="crud" >
+        <ul class="titulo">
+          <li class="right">   
+            <a href="#Consultar">Consultar</a></li>
+          <li class="right">
+              <a href="#Incluir">Incluir</a></li>
+          <li class="right">
+              <a href="#Excluir">Excluir</a></li>
+        </ul>  -->
+            
+        <!-- <ul class="navbar-nav"> 
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Consultar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Incluir</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Excluir</a> 
+        </nav> -->
+
+        
+    
+
+    
+        
+        
+
+    <form action="insercao_organizacao.php" method="GET">
+    <div id="div1">
+	<label for="idNomeOrg">
+		<input type="text" name="nome_org" id="idNomeOrg" placeholder="Nome da Organização" class="form-control mb-1 mr-sm-2">
+	</label>
+	<br>        
+	<label for="idNomeOrgPai">
+		<input type="text" name="id_organizacao_pai" id="idNomeOrgPai" placeholder="Organização-Pai (selecione)" 
+                 class="form-control mb-1 mr-sm-2">
+	</label>
+	<br>
+        <!-- <select id="idTipoOrg"> -->
+        <label for="idTipoOrg">    
+		<input type="text" name="TipoOrg" placeholder="Tipo da Organização (selecione)" class="form-control mb-1 mr-sm-2">
+	<!-- </select> -->
+    </div>
+    <br>
+
+        
+<!--    <div id="div2">
+           
+        <label for="idUFOrg"></label>
+                <!-- <input type="text" name="FALTANDO" id="idUFOrg" placeholder="Nome da Organização"> -->
+<!--        <br>
+        <label for="idCEPOrg">
+        	<input type="number" name="CEP_org" id="idCEPOrg" placeholder="CEP ()" class="form-control mb-1 mr-sm-2">
+        </label>
+        <br>        
+        <label for="idCidadeOrg">
+        	<input type="text" name="id_cidade_org" id="idCidadeOrg" placeholder="Cidade ()" class="form-control mb-1 mr-sm-2">
+        </label>
+        <br>
+	<label for="idLogradouroOrg">
+		<input type="text" name="logradouro_org" id="idLogradouroOrg" placeholder="Logradouro ()" class="form-control mb-1 mr-sm-2">
+	</label>	
+	<br>
+        <label for="idNumLogradouroOrg">
+            <input type="number" name="num_org" id="idLogradouroOrg" placeholder="Número ()" class="form-control mb-1 mr-sm-2">
+        </label>
+        <br>
+        <label for="idComplementoOrg">
+        	<input type="text" name="complemento_org" id="idComplementoOrg" placeholder="Complemento ()" 
+                 class="form-control mb-1 mr-sm-2">
+        </label>        
+        <br>
+        <label for="idBairroOrg">
+        	<input type="text" name="bairro_org" id="idBairroOrg" placeholder="Bairro ()" class="form-control mb-1 mr-sm-2">
+        </label>
+        <br>
+        
+    </div> -->
+    <br>
+        
+
+<!--    <div id="div3">        
+	<label for="idEmailOrg">
+		<input type="email" name="EmailOrg" id="idEmailOrg" placeholder="E-mail da Organização ()" 
+                 class="form-control mb-1 mr-sm-2">
+	</label>	
+	<br>
+	<label for="idFoneFixoOrg">
+		<input type="tel" name="FoneFixoOrg" id="idFoneOrg" placeholder="Fone fixo da Organização ()" 
+                 class="form-control mb-1 mr-sm-2">
+	</label>
+        <br>
+        <label for="idFoneCelOrg">
+		<input type="tel" name="FoneCelOrg" id="idFoneOrg" placeholder="Celular da Organização ()" 
+                 class="form-control mb-1 mr-sm-2">
+	</label>
+	<br> 
+    </div> -->
+
+        
+        
+        
+        
+<?php
+/*
+    # cria conexão com banco de dados
+    include_once "../inc/conectabd.php";
+    include_once "../inc/funcoes.inc.php";  //VERIFICAR
+    monta_organizacao($conn);               // CRIAR FUNÇAO
+ 
+ */
+    ?>
+    </form>
+        
+        
+        
+        
+</body>
+</html>
+  
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * and open the template in the editor.
+ */
+
+
+
+
+
+
+
+
+
+/*
+  try {
+   
+  include_once "../inc/conectabd.inc.php";
+
+  echo "<h3>BSGI (PDO)</h3>";
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    } catch (PDOException $e){
+      echo "Erro: ". $e->getMessage();
+      }
+ * 
+ */
+?> 
